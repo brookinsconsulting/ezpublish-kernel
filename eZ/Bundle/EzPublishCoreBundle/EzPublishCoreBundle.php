8da46719daf5590748b92785ac28ed62ee9a73c5
Jérôme Vieilledent <jv@ez.no>
2014-10-09 15:33:32 +0200

    EZP-22400: Implemented a factory to build Varnish proxy client
    This makes it possible to merge purge servers defined in eZ SiteAccess aware config with the one defined at FOSHttpCacheBundle levels

--- eZ/Bundle/EzPublishCoreBundle/EzPublishCoreBundle.php
+++ eZ/Bundle/EzPublishCoreBundle/EzPublishCoreBundle.php
@@ -13,6 +13,7 @@ use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\AsseticPass;
 use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\ConfigResolverParameterPass;
 use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\FieldTypeParameterProviderRegistryPass;
 use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\FragmentPass;
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\HttpCachePass;
 use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\IdentityDefinerPass;
 use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\ImaginePass;
 use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\RegisterStorageEnginePass;
@@ -67,6 +68,7 @@ class EzPublishCoreBundle extends Bundle
         $container->addCompilerPass( new FragmentPass );
         $container->addCompilerPass( new StorageConnectionPass );
         $container->addCompilerPass( new ImaginePass );
+        $container->addCompilerPass( new HttpCachePass );
         $container->addCompilerPass( new ConfigResolverParameterPass( new DynamicSettingParser() ) );
         $container->addCompilerPass( new AsseticPass() );
 
